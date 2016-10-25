8138963195824730b9d5726663a1f35fadab5c66
Tony1213 <1973265001@qq.com>
2016-10-25 10:02:34 +0800

    提交Test部分代码

---
+++ app/src/main/java/com/robot/et/test/fresco/FrescoActivity.java
@@ -0,0 +1,20 @@
+package com.robot.et.test.fresco;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.robot.et.R;
+
+public class FrescoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fresco);
+        Uri uri = Uri.parse("http://i.imgur.com/DvpvklR.png");
+        SimpleDraweeView draweeView = (SimpleDraweeView) findViewById(R.id.image_view);
+        draweeView.setImageURI(uri);
+    }
+}
